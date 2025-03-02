117270531248e132b2b2958f2f906c5fbdf600b3
root <mohammad.yasser.a.98@gmail.com>
2025-03-02 15:09:52 +0300

    Schema2

--- models/movies.js
+++ models/movies.js
@@ -1,61 +1,15 @@
 const mongoose = require('mongoose');
-
-
-
-
-//reviews schema
-
-
-
-
-//movies Schema
-
-const  moviesSchema = new mongoose.Schema({
-
-title:{
-type:String,
-required:true,
-},
-genre:{
-    type:String,
-    required:true,
-    enum:['Action', 'Comedy','Darama','Horror','Sci-Fi']
-},
-
-description:{
-
-type:String,
-required:true,
-
-},
-director:{
-
-    type:String
-},
-releaseDate:{
- type:Date
-
-    
-},
-
- author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
- reviews:[reviewsSchema],
-
-},    { timestamps: true }
-);
-
-const Movie =mongoose.model('Movie', moviesSchema);
-module.exports=Movie
-
-
-
-
-
-
-
-
-
-
-
-
-
+const commentSchema = new mongoose.Schema(
+    {
+      text: {
+        type: String,
+        required: true
+      },
+      rating: {
+        type: Number,
+        required: true
+      },
+      author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    },
+    { timestamps: true }
+  );
